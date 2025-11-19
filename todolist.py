5dfccc5e05bcc4f4967ce4eaba5817ce19b9c7bd
Sushi922 <sngaarna@gmail.com>
2025-11-19 18:34:49 +0000

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -8,20 +8,26 @@ while True:
     print("3.delete a task")
     print("4.exit")
 
-    Choice=int(input("choose an option(1-4):"))
+    Choice=int(input("choose an option(1-4): "))
 
     if Choice==1:
         for i in task:
             print(i)
 
     elif Choice==2:
-        Choicee=input("Enter the task")
+        Choicee=input("Enter the task ")
         task.append(Choicee)
         print(Choicee+" added!")
     elif Choice==3:
-        pass
+        discard=input("Enter the task to be removed: ")
+        task.remove(discard)
+        print(discard+" removed!")
     elif Choice==4:
-        pass
+        print("Exiting the todo list")
+        break
     else:
-        pass
+        print("invalid choice, please choose between 1,2,3 and 4: ")
+
+
+
 
